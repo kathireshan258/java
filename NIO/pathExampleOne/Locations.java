2dd0add1c595e30ebd49111c608aaaa0c568e0af
Kathireshan Manoharan <kathireshan258@gmail.com>
2022-04-03 20:26:51 +0530

    Java NIO Overview

---
+++ NIO/pathExampleOne/Locations.java
@@ -0,0 +1,116 @@
+package NIO.pathExampleOne;
+
+import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+class Locations implements Map<Integer, Location> {
+    private static Map<Integer, Location> locations = new LinkedHashMap<>();
+
+    static {
+
+       try (ObjectInputStream locFile = new ObjectInputStream(new BufferedInputStream(new FileInputStream("NIO//pathExampleOne//locations.dat")))) {
+            boolean eof = false;
+            while (!eof) {
+                try {
+                    Location location = (Location) locFile.readObject();
+                    System.out.println("Read Location " + location.getLocationID() + ": " + location.getDescription());
+                    System.out.println("Found " + location.getExits().size() + " exists");
+
+                    locations.put(location.getLocationID(), location);
+                } catch (InvalidClassException ic) {
+                    System.out.println("Invalid class Exception: " + ic.getMessage());
+                } catch (ClassNotFoundException ce){
+                    System.out.println("Class Not Found Exception " + ce.getMessage());
+                } catch (EOFException eo) {
+                    eof = true;
+                }
+            }
+       } catch (IOException e) {
+           System.out.println();
+       }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Path locPath = FileSystems.getDefault().getPath("NIO//pathExampleOne//locations_big.txt");
+        Path dirPath = FileSystems.getDefault().getPath("directions_big.txt");
+        try (BufferedWriter locFile = Files.newBufferedWriter(locPath);
+             BufferedWriter dirFile = Files.newBufferedWriter(dirPath)) {
+             for (Location location : locations.values()) {
+                 locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+                 for (String direction : location.getExits().keySet()) {
+                     if (!direction.equalsIgnoreCase("Q")) {
+                         dirFile.write(location.getLocationID() + "," + direction + "," + location.getExits().
+                                 get(direction) + "\n");
+                     }
+                 }
+             }
+        }
+    }
+        @Override
+    public int size() {
+        return locations.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return locations.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return locations.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return locations.containsValue(value);
+    }
+
+    @Override
+    public Location get(Object key) {
+        return locations.get(key);
+    }
+
+    @Override
+    public Location put(Integer key, Location value) {
+        return locations.put(key, value);
+    }
+
+    @Override
+    public Location remove(Object key) {
+        return locations.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Integer, ? extends Location> m) {
+        locations.putAll(m);
+    }
+
+    @Override
+    public void clear() {
+        locations.clear();
+    }
+
+    @Override
+    public Set<Integer> keySet() {
+        return locations.keySet();
+    }
+
+    @Override
+    public Collection<Location> values() {
+        return locations.values();
+    }
+
+    @Override
+    public Set<Entry<Integer, Location>> entrySet() {
+        return locations.entrySet();
+    }
+}
